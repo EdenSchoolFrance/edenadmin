da77d99dfd6768e9fc5bea797de9429f71db4577
DOTG29 <dgoris@edenschool.fr>
2023-05-30 15:18:08 +0200

    update home page

--- client/app/routes/index.tsx
+++ client/app/routes/index.tsx
@@ -1,39 +1,13 @@
-import { CardBody, Card, Stack, Image, Heading, Text, Divider, CardFooter, ButtonGroup, Button } from "@chakra-ui/react";
+import { StorieCard } from "~/components/StorieCard";
 
 const Index = () => {
   return (
-    <Card maxW='sm' ml="100px" mt='200px'>
-    <CardBody>
-      <Image
-        src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-        alt='Green double couch with wooden legs'
-        borderRadius='lg'
-      />
-      <Stack mt='6' spacing='3'>
-        <Heading size='md'>Living room Sofa</Heading>
-        <Text>
-          This sofa is perfect for modern tropical spaces, baroque inspired
-          spaces, earthy toned spaces and for people who love a chic design with a
-          sprinkle of vintage design.
-        </Text>
-        <Text color='blue.600' fontSize='2xl'>
-          $450
-        </Text>
-      </Stack>
-    </CardBody>
-    <Divider />
-    <CardFooter>
-      <ButtonGroup spacing='2'>
-        <Button variant='solid' colorScheme='blue'>
-          Buy now
-        </Button>
-        <Button variant='ghost' colorScheme='blue'>
-          Add to cart
-        </Button>
-      </ButtonGroup>
-    </CardFooter>
-  </Card>
-
+<>
+    <StorieCard storieId={1} storieTitle="yet" storieDescription="ret" storieImgSrc="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"/>
+    <StorieCard storieId={1} storieTitle="yet" storieDescription="ret" storieImgSrc="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"/>
+    <StorieCard storieId={1} storieTitle="yet" storieDescription="ret" storieImgSrc="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"/>
+    <StorieCard storieId={1} storieTitle="yet" storieDescription="ret" storieImgSrc="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"/>
+</>
   );
 };
 
