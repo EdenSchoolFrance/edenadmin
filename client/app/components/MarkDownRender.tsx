068b21ecd2fd582421bf2ab2e50b114d15ef3c55
jokay03J-V2 <jmagro@edenschool.fr>
2023-06-02 15:42:33 +0200

    fix config

--- client/app/components/MarkDownRender.tsx
+++ client/app/components/MarkDownRender.tsx
@@ -38,36 +38,47 @@ const TableRender = ({
   );
 };
 
-export const MarkDownRender = ({ children, ...props }: IMarkdownRender) => {
+export const MarkDownRender = ({
+  children,
+  options,
+  ...props
+}: IMarkdownRender) => {
   return (
     <Markdown
-      options={{
-        overrides: {
-          h1: { component: Heading, props: { as: "h1", size: "4xl" } },
-          h2: { component: Heading, props: { as: "h2", size: "3xl" } },
-          h3: { component: Heading, props: { as: "h3", size: "2xl" } },
-          h4: { component: Heading, props: { as: "h4", size: "xl" } },
-          h5: { component: Heading, props: { as: "h5", size: "lg" } },
-          h6: { component: Heading, props: { as: "h6", size: "md" } },
-          p: { component: Text, props: { as: "p", size: "lg" } },
-          ul: { component: UnorderedList },
-          ol: { component: OrderedList },
-          li: { component: ListItem, props: { as: "li", fontSize: "lg" } },
-          table: {
-            component: TableRender,
-            props: { variant: "simple" },
-          },
-          caption: { component: TableCaption },
-          thead: { component: Thead },
-          tr: { component: Tr },
-          th: { component: Th },
-          tbody: { component: Tbody },
-          td: { component: Td },
-          tfoot: { component: Tfoot },
-          img: Img,
-          a: { component: Link, props: { color: "teal.600" } },
-        },
-      }}
+      options={
+        options
+          ? options
+          : {
+              overrides: {
+                h1: { component: Heading, props: { as: "h1", size: "4xl" } },
+                h2: { component: Heading, props: { as: "h2", size: "3xl" } },
+                h3: { component: Heading, props: { as: "h3", size: "2xl" } },
+                h4: { component: Heading, props: { as: "h4", size: "xl" } },
+                h5: { component: Heading, props: { as: "h5", size: "lg" } },
+                h6: { component: Heading, props: { as: "h6", size: "md" } },
+                p: { component: Text, props: { as: "p", size: "lg" } },
+                ul: { component: UnorderedList },
+                ol: { component: OrderedList },
+                li: {
+                  component: ListItem,
+                  props: { as: "li", fontSize: "lg" },
+                },
+                table: {
+                  component: TableRender,
+                  props: { variant: "simple" },
+                },
+                caption: { component: TableCaption },
+                thead: { component: Thead },
+                tr: { component: Tr },
+                th: { component: Th },
+                tbody: { component: Tbody },
+                td: { component: Td },
+                tfoot: { component: Tfoot },
+                img: Img,
+                a: { component: Link, props: { color: "teal.600" } },
+              },
+            }
+      }
       {...props}
     >
       {children}
